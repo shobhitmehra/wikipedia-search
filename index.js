acd0a76999313dd49d78d85f0a51c5161e1d6abc
Shobhit Mehra <shobhitmehra1@gmail.com>
2018-05-10 19:04:45 +0530

    Update index.js

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ server.use(bodyParser.json());
 server.get('/get-movie-details', (req, res) => {
 
     const movieToSearch = req.body.result && req.body.result.parameters && req.body.result.parameters.movie ? req.body.result.parameters.movie : 'The Godfather';
-    const reqUrl = encodeURI(`http://www.omdbapi.com/?t=${movieToSearch}&apikey=${API_KEY}`);
+    const reqUrl = encodeURI("http://www.omdbapi.com/?t=${movieToSearch}&apikey=${API_KEY});
     http.get(reqUrl, (responseFromAPI) => {
         let completeResponse = '';
         responseFromAPI.on('data', (chunk) => {
@@ -25,8 +25,8 @@ server.get('/get-movie-details', (req, res) => {
         });
         responseFromAPI.on('end', () => {
             const movie = JSON.parse(completeResponse);
-            let dataToSend = movieToSearch === 'The Godfather' ? `I don't have the required info on that. Here's some info on 'The Godfather' instead.\n` : '';
-            dataToSend += `${movie.Title} is a ${movie.Actors} starer ${movie.Genre} movie, released in ${movie.Year}. It was directed by ${movie.Director}`;
+            let dataToSend = movieToSearch === 'The Godfather' ? 'I don't have the required info on that. Here's some info on 'The Godfather' instead.\n' : '';
+            dataToSend += '${movie.Title} is a ${movie.Actors} starer ${movie.Genre} movie, released in ${movie.Year}. It was directed by ${movie.Director}';
 
             return res.json({
                 speech: dataToSend,
