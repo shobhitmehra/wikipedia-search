df46566738d670dd3e8761cbe98d49fe1ed349ba
Shobhit Mehra <shobhitmehra1@gmail.com>
2018-05-10 17:36:05 +0530

    Update index.js

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ server.use(bodyParser.urlencoded({
 server.use(bodyParser.json());
 
 
-server.post('/get-movie-details', (req, res) => {
+server.get('/get-movie-details', (req, res) => {
 
     const movieToSearch = req.body.result && req.body.result.parameters && req.body.result.parameters.movie ? req.body.result.parameters.movie : 'The Godfather';
     const reqUrl = encodeURI(`http://www.omdbapi.com/?t=${movieToSearch}&apikey=${API_KEY}`);
