63eae4d6f7a420445a96cfbf494a87189d750f75
Shobhit Mehra <shobhitmehra1@gmail.com>
2018-05-11 16:17:59 +0530

    Update index.js

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ server.post('/get-movie-details', (req, res) => {
             const movie = JSON.parse(completeResponse);
             let dataToSend = movieToSearch === 'The Godfather' ? 'I dont have the required info on that. Heres some info on The Godfather instead.\n' : '';
             dataToSend += '${movie.Title} is a ${movie.Actors} starer ${movie.Genre} movie, released in ${movie.Year}. It was directed by ${movie.Director}';
-			dataToSend +=movie.Director;
+			dataToSend +={movie.Director};
 
             return res.json({
                 speech: dataToSend,
