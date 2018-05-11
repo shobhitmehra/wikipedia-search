02b9e20a1c1169f9ce78ce70352f32bc14a289bd
Shobhit Mehra <shobhitmehra1@gmail.com>
2018-05-11 17:16:39 +0530

    Update index.js

--- index.js
+++ index.js
@@ -26,10 +26,8 @@ server.post('/get-movie-details', (req, res) => {
         responseFromAPI.on('end', () => {
             var movie = JSON.parse(completeResponse);
 		
-            // var dataToSend = movieToSearch === 'The Godfather' ? `I don't have the required info on that. Here's some info on 'The Godfather' instead.\n` : '';
-           
-			var dataToSend = movie;
-		   //var dataToSend ="makasakinaka";
+           let dataToSend = movieToSearch === 'The Godfather' ? `I don't have the required info on that. Here's some info on 'The Godfather' instead.\n` : '';
+            dataToSend += `${movie.Title} is a ${movie.Actors} starer ${movie.Genre} movie, released in ${movie.Year}. It was directed by ${movie.Director}`;
 
 			
 
