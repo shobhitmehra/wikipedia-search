afacb63203c65109c215971f3283f74c739f2e87
Shobhit Mehra <shobhitmehra1@gmail.com>
2018-05-11 16:43:35 +0530

    Update index.js

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ server.post('/get-movie-details', (req, res) => {
              let dataToSend = movieToSearch === 'The Godfather' ? `I don't have the required info on that. Here's some info on 'The Godfather' instead.\n` : '';
            
 			//let dataToSend = movie.Title;
-		   dataToSend += `movie.Title is a ${movie.Actors} starer ${movie.Genre} movie, released in ${movie.Year}. It was directed by ${movie.Director}`;
+		   dataToSend += movie.Title +`is a ${movie.Actors} starer ${movie.Genre} movie, released in ${movie.Year}. It was directed by ${movie.Director}`;
 
 			
 
